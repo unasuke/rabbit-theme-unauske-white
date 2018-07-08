a1cccce33c85534cbb22424ecb6c28f41e7f36f8
Yusuke Nakamura <yusuke1994525@gmail.com>
2018-07-08 17:38:47 +0900

    Setup font

--- theme.rb
+++ theme.rb
@@ -2,6 +2,28 @@
 @default_background ||= @background
 @default_shadow_color ||= @shadow_color
 
+@default_font          = 'Noto Sans CJK JP'
+@font_family           = find_font_family(@default_font)
+@bold_font             = @default_font
+@bold_font_family      = find_font_family(@bold_font)
+@monospace_font        = 'Cica'
+@monospace_font_family = find_font_family(@monospace_font)
+
+@xxxx_large_font_size = screen_size(10 * Pango::SCALE)
+@xxx_large_font_size  = screen_size(8 * Pango::SCALE)
+@xx_large_font_size   = screen_size(6 * Pango::SCALE)
+@x_large_font_size    = screen_size(5 * Pango::SCALE)
+@large_font_size      = screen_size(4.5 * Pango::SCALE)
+@normal_font_size     = screen_size(3.5 * Pango::SCALE)
+@small_font_size      = screen_size(3.2  * Pango::SCALE)
+@x_small_font_size    = screen_size(3 * Pango::SCALE)
+@xx_small_font_size   = screen_size(2.8 * Pango::SCALE)
+@xxx_small_font_size  = screen_size(2.5 * Pango::SCALE)
+@script_font_size            = @x_small_font_size
+@large_script_font_size      = @small_font_size
+@x_large_script_font_size    = @large_font_size
+@title_slide_title_font_size = @xxx_large_font_size
+
 set_foreground(@default_foreground)
 set_background(@default_background)
 
@@ -31,3 +53,9 @@ include_theme("tag")
 include_theme("syntax-highlighting")
 
 include_theme("default-comment")
+
+
+match(TitleSlide, Title) do |titles|
+  titles.padding_bottom = @space * 2
+  titles.prop_set("weight", "SemiBold")
+end
